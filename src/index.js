b0312e520ad0bf8b3243cdb203693af89938c59a
lidrail35 <lidrail35@mail.ru>
2023-02-02 13:09:00 +0300

    feat: add solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, brConfig) {
+let arrBr = []
+let fBr = undefined;
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j=0; j<brConfig.length; j++)  {
+      //console.log(brConfig, brConfig[j], str, str[i]);
+      fBr = brConfig[j].indexOf(str[i]);
+
+      if (fBr<0) {continue}
+      if (fBr === 0) {
+        ((str[i]===brConfig[j][1]) &&(str[i]===arrBr[arrBr.length-1])) ? arrBr.pop() : arrBr.push(str[i]);
+      }
+        else 
+            if ((fBr === 1) && (arrBr.length>0))  {
+              if (arrBr[arrBr.length-1] === brConfig[j][0]) {
+                arrBr.pop();
+              } else return false;
+            }  else return false;
+    }
+  }
+ return (arrBr.length === 0) ? true : false; // your solution
 }
